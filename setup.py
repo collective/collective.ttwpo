13ecd0fb969b8d289de75b6710b044c61d931224
Jens W. Klein <jk@kleinundpartner.at>
2017-11-02 14:59:12 +0100

    declarative setup

--- setup.py
+++ setup.py
@@ -1,58 +1,10 @@
 # -*- coding: utf-8 -*-
-"""Installer for the collective.zanata package."""
-
-from setuptools import find_packages
+"""Installer for the example.package package."""
 from setuptools import setup
 
-
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
-
-
 setup(
-    name='collective.zanata',
-    version='1.0a1',
-    description="Zanata Translation Service Integration with Plone",
-    long_description=long_description,
-    # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Framework :: Plone :: 5.1",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-    ],
-    keywords='Python Plone Translation PO gettext zanata',
-    author='Jens W. Klein',
-    author_email='jk@kleinundpartner.at',
-    url='https://pypi.python.org/pypi/collective.zanata',
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['collective'],
+    # thanks to this bug
+    # https://github.com/pypa/setuptools/issues/1136
+    # we need one line in here:
     package_dir={'': 'src'},
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'plone.api',
-        'Products.CMFPlone',
-        'PyYAML',
-        'requests',
-        'setuptools',
-    ],
-    extras_require={
-        'test': [
-            'plone.app.testing',
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes[test]'
-        ],
-    },
-    entry_points="""
-    [z3c.autoinclude.plugin]
-    target = plone
-    """,
 )
